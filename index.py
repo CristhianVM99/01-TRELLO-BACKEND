6ca6309335305ddd6e2db504d7074c946416f3f1
Cristhian Villca Mamani <cristhian.villca@tugerente.com>
2024-01-04 02:55:23 -0400

    agregando requirements.txt

--- index.py
+++ index.py
@@ -2,17 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# TODO: pagina principal.
+#TODO: pagina principal.
 @app.route('/')
 def index():
     return "<h1>Bienvenido a la pagina principal.</h1>"
 
-# TODO: pagina de contacto.
+#TODO: pagina de contacto.
 @app.route('/Contact')
 def contacto():
     return "<h1>Contacte con Nosotros.</h1>"
 
-# TODO: pagina sobre nosotros.
+#TODO: pagina sobre nosotros.
 @app.route('/SobreNosotros')
 def sobreNosotros():
     return "<h1>Información sobre la app.</h1>"
